8ba9cc7f53922703109b7b7a11a78f7865d501f3
nahid35 <nahidul.cse@gmail.com>
2019-07-30 14:47:55 +0600

    Added query filter related trait and class

---
+++ app/Traits/Filterable.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait Filterable
+{
+    /**
+     * Filter a result set.
+     *
+     * @param  Builder      $query
+     * @param  QueryFilters $filters
+     * @return Builder
+     */
+    public function scopeFilter($query, QueryFilters $filters)
+    {
+        return $filters->apply($query);
+    }
+}
